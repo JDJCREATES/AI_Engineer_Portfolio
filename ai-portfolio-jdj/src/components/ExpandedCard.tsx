29afc920ce825dc9290024dfa3a85e7c08e65c51
JDJCREATES <jdj.creates@gmail.com>
2025-12-14 14:14:43 -0600

    updated ExpandedCard.tsx

--- ai-portfolio-jdj/src/components/ExpandedCard.tsx
+++ ai-portfolio-jdj/src/components/ExpandedCard.tsx
@@ -1,16 +1,21 @@
 import '.././ExpandedCardStyles.tsx';
 
-const ExpandedCard = () => {
+const ExpandedCard = ( props) => {
   
   return (
     <section className="expanded-card-section">
       <div className="demo-video-container">
         <video className="demo-video">
-          <source src="" />
+          <source src={props.demoSrc1}  />
         </video>
       </div>
-      <div className="">
-        
+      <div className="project-details">
+        <h2 className="project-title">
+          {props.title}
+        </h2>
+        <p className="project-description">
+          {props.description}
+        </p>
       </div>
     </section>
   );
